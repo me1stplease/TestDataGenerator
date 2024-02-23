251bb9ed71c4cc11b5d33b9f03142a5d855bd430
me1stplease <arman.khan152@gmail.com>
2024-02-24 02:26:39 +0530

    Change

--- DGenUI.py
+++ DGenUI.py
@@ -1,12 +1,19 @@
+import datetime
+
 import streamlit as st
 from faker import Faker
 import csv
 from Transpose import transpose as tps
 
-
 from DataGen import Name as nm
 from DataGen import Phone as ph
 from DataGen import Email as em
+from DataGen import Amount as amt
+from DataGen import Text as txt
+from DataGen import Address as addrs
+from DataGen import Date as dt
+from DataGen import Custom as cstm
+from DataGen import Categorical as ctg
 
 global datatype
 global columnName
@@ -14,7 +21,6 @@ global property1
 global property2
 global property3
 global property4
-
 
 records = 10
 
@@ -28,7 +34,7 @@ with st.container(border=True):
     with col1:
         datatype = st.selectbox(
             'Data Type: ',
-            ('Name', 'Phone', 'Email'))
+            ('Name', 'Phone', 'Email', 'Amount', 'Text', 'Address', 'Date', 'Custom', 'Categorical'))
 
     with col2:
         columnName = st.text_input('Attribute Name: ', 'Text')
@@ -44,14 +50,58 @@ with st.container(border=True):
             property4 = ''
 
         if datatype == "Phone":
-            plen = ''
-            property1 = plen
+            pformat = st.text_input('Number Format: ', '+##-##########')
+            property1 = pformat
             property2 = ''
             property3 = ''
             property4 = ''
 
-        else:
+        if datatype == "Email":
+            mprovider = st.text_input('Mail Providers: ', '@gmail.com, @yahoo.com, @hotmail.com')
+            property1 = mprovider
+            property2 = ''
+            property3 = ''
+            property4 = ''
+
+        if datatype == "Amount":
+            minV = st.text_input('Minimum Value: ', '0')
+            maxV = st.text_input('Maximum Value: ', '999999')
+            property1 = minV
+            property2 = maxV
+            property3 = ''
+            property4 = ''
+
+        if datatype == "Text":
+            tlen = st.text_input('Text Length: ', '10')
+            property1 = tlen
+            property2 = ''
+            property3 = ''
+            property4 = ''
+
+        if datatype == "Address":
             property1 = ''
+            property2 = ''
+            property3 = ''
+            property4 = ''
+
+        if datatype == "Date":
+            startd = st.date_input("Start Date", datetime.date(1914, 7, 7))
+            endd = st.date_input("Start Date", datetime.date(2024, 7, 7))
+            property1 = startd
+            property2 = endd
+            property3 = ''
+            property4 = ''
+
+        if datatype == "Custom":
+            cinput = st.text_input('Custom Value: ', 'ABC')
+            property1 = cinput
+            property2 = ''
+            property3 = ''
+            property4 = ''
+
+        if datatype == "Categorical":
+            cinput = st.text_input('Category List: ', 'A,B,C')
+            property1 = cinput
             property2 = ''
             property3 = ''
             property4 = ''
@@ -62,7 +112,7 @@ with col1:
     if st.button("Add Attribute", type="primary"):
         f = open("input.csv", "a")
         f.write(
-            datatype + "," + columnName + "," + property1 + "," + property2 + "," + property3 + "," + property4 + "\n")
+            str(datatype) + "," + str(columnName) + "," + str(property1) + "," + str(property2) + "," + str(property3) + "," + str(property4) + "\n")
         f.close()
         st.success("Attribute has been added")
 
@@ -87,16 +137,44 @@ if st.button("Generate Data", type="primary"):
 
     for i in range(1, len(data)):
         if data[i][0] == "Name":
-            name = nm(fake, records, data[i][0])
+            name = nm(fake, records, data[i][2])
             output_record.append(name)
 
         if data[i][0] == "Phone":
-            phone = ph(fake, records)
+            phone = ph(fake, records, data[i][2])
             output_record.append(phone)
 
         if data[i][0] == "Email":
-            email = em(fake, records)
+            email = em(fake, records, data[i][2])
             output_record.append(email)
+
+        if data[i][0] == "Amount":
+            amount = amt(records, float(data[i][2]), float(data[i][3]))
+            output_record.append(amount)
+
+        if data[i][0] == "Text":
+            text = txt(fake, records, data[i][2])
+            output_record.append(text)
+
+        if data[i][0] == "Address":
+            address = addrs(fake, records)
+            output_record.append(address)
+
+        if data[i][0] == "Date":
+            date = dt(fake, records, str(data[i][2]), str(data[i][3]))
+            output_record.append(date)
+
+        if data[i][0] == "Custom":
+            custom = cstm(records, data[i][2])
+            output_record.append(custom)
+
+        if data[i][0] == "Categorical":
+            categorical = ctg(fake, records, data[i][2])
+            output_record.append(categorical)
+
+    # f = open("input.csv", "w")
+    # f.write("datatype,columnName,property1,property2,property3,property4\n")
+    # f.close()
 
     # print("final_op:")
     # print(output_record)
@@ -113,4 +191,3 @@ if st.button("Generate Data", type="primary"):
     for data_list in Tp_output:
         writer.writerow(data_list)
     myFile.close()
-
