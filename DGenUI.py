6d4fc11f2bd5f71c007a7682d9494fd9c4ba37ec
me1stplease <arman.khan152@gmail.com>
2024-02-25 14:10:05 +0530

    SynData Add

--- DGenUI.py
+++ DGenUI.py
@@ -3,7 +3,9 @@ import datetime
 import streamlit as st
 from faker import Faker
 import csv
+import pandas as pd
 from Transpose import transpose as tps
+from SyntheticDataUI import SDUI
 
 from DataGen import Name as nm
 from DataGen import Phone as ph
@@ -22,172 +24,187 @@ global property2
 global property3
 global property4
 
-records = 10
+global records
 
 # f = open("input.csv", "w")
-# f.write("datatype,columnName,property1,property2,property3,property4\n")
+# f.write("datatype|columnName|property1|property2|property3|property4\n")
 # f.close()
 
-with st.container(border=True):
-    col1, col2, col3 = st.columns(3)
+tab1, tab2 = st.tabs(["Test Data Creation", "Synthetic Data Generation"])
 
-    with col1:
-        datatype = st.selectbox(
-            'Data Type: ',
-            ('Name', 'Phone', 'Email', 'Amount', 'Text', 'Address', 'Date', 'Custom', 'Categorical'))
+with tab1:
+    with st.container(border=True):
+        col1, col2, col3 = st.columns(3)
 
-    with col2:
-        columnName = st.text_input('Attribute Name: ', 'Text')
+        with col1:
+            datatype = st.selectbox(
+                'Data Type: ',
+                ('Name', 'Phone', 'Email', 'Amount', 'Text', 'Address', 'Date', 'Custom', 'Categorical'))
 
-    with col3:
-        if datatype == "Name":
-            ntype = st.selectbox(
-                'Type: ',
-                ('Full Name', 'First', 'Last'))
-            property1 = ntype
-            property2 = ''
-            property3 = ''
-            property4 = ''
+        with col2:
+            columnName = st.text_input('Attribute Name: ', 'Text')
 
-        if datatype == "Phone":
-            pformat = st.text_input('Number Format: ', '+##-##########')
-            property1 = pformat
-            property2 = ''
-            property3 = ''
-            property4 = ''
+        with col3:
+            if datatype == "Name":
+                ntype = st.selectbox(
+                    'Type: ',
+                    ('Full Name', 'First', 'Last'))
+                property1 = ntype
+                property2 = ''
+                property3 = ''
+                property4 = ''
 
-        if datatype == "Email":
-            mprovider = st.text_input('Mail Providers: ', '@gmail.com, @yahoo.com, @hotmail.com')
-            property1 = mprovider
-            property2 = ''
-            property3 = ''
-            property4 = ''
+            if datatype == "Phone":
+                pformat = st.text_input('Number Format: ', '+##-##########')
+                property1 = pformat
+                property2 = ''
+                property3 = ''
+                property4 = ''
 
-        if datatype == "Amount":
-            minV = st.text_input('Minimum Value: ', '0')
-            maxV = st.text_input('Maximum Value: ', '999999')
-            property1 = minV
-            property2 = maxV
-            property3 = ''
-            property4 = ''
+            if datatype == "Email":
+                mprovider = st.text_input('Mail Providers: ', '@gmail.com, @yahoo.com, @hotmail.com')
+                property1 = mprovider
+                property2 = ''
+                property3 = ''
+                property4 = ''
 
-        if datatype == "Text":
-            tlen = st.text_input('Text Length: ', '10')
-            property1 = tlen
-            property2 = ''
-            property3 = ''
-            property4 = ''
+            if datatype == "Amount":
+                minV = st.text_input('Minimum Value: ', '0')
+                maxV = st.text_input('Maximum Value: ', '999999')
+                property1 = minV
+                property2 = maxV
+                property3 = ''
+                property4 = ''
 
-        if datatype == "Address":
-            property1 = ''
-            property2 = ''
-            property3 = ''
-            property4 = ''
+            if datatype == "Text":
+                tlen = st.text_input('Text Length: ', '10')
+                property1 = tlen
+                property2 = ''
+                property3 = ''
+                property4 = ''
 
-        if datatype == "Date":
-            startd = st.date_input("Start Date", datetime.date(1914, 7, 7))
-            endd = st.date_input("Start Date", datetime.date(2024, 7, 7))
-            property1 = startd
-            property2 = endd
-            property3 = ''
-            property4 = ''
+            if datatype == "Address":
+                property1 = ''
+                property2 = ''
+                property3 = ''
+                property4 = ''
 
-        if datatype == "Custom":
-            cinput = st.text_input('Custom Value: ', 'ABC')
-            property1 = cinput
-            property2 = ''
-            property3 = ''
-            property4 = ''
+            if datatype == "Date":
+                startd = st.date_input("Start Date", datetime.date(1914, 7, 7))
+                endd = st.date_input("Start Date", datetime.date(2024, 7, 7))
+                property1 = startd
+                property2 = endd
+                property3 = ''
+                property4 = ''
 
-        if datatype == "Categorical":
-            cinput = st.text_input('Category List: ', 'A,B,C')
-            property1 = cinput
-            property2 = ''
-            property3 = ''
-            property4 = ''
+            if datatype == "Custom":
+                cinput = st.text_input('Custom Value: ', 'ABC')
+                property1 = cinput
+                property2 = ''
+                property3 = ''
+                property4 = ''
 
-col1, col2 = st.columns(2)
+            if datatype == "Categorical":
+                cinput = st.text_input('Category List: ', 'A,B,C')
+                property1 = cinput
+                property2 = ''
+                property3 = ''
+                property4 = ''
 
-with col1:
+    # col1, col2 = st.columns(2)
+    #
+    # with col1:
     if st.button("Add Attribute", type="primary"):
         f = open("input.csv", "a")
         f.write(
-            str(datatype) + "," + str(columnName) + "," + str(property1) + "," + str(property2) + "," + str(property3) + "," + str(property4) + "\n")
+            str(datatype) + "|" + str(columnName) + "|" + str(property1) + "|" + str(property2) + "|" + str(
+                property3) + "|" + str(property4) + "\n")
         f.close()
         st.success("Attribute has been added")
+        df = pd.read_csv('input.csv', delimiter='|')
+        st.table(df)
 
-with col2:
+    # with col2:
     if st.button("Clear All", type="secondary"):
         f = open("input.csv", "w")
-        f.write("datatype,columnName,property1,property2,property3,property4\n")
+        f.write("datatype|columnName|property1|property2|property3|property4\n")
         f.close()
 
-st.divider()
+    try:
+        records = int(st.text_input('Number of records to be generated: ', '10'))
 
-if st.button("Generate Data", type="primary"):
-    global data
-    with open('input.csv', 'r') as f:
-        reader = csv.reader(f)
-        data = list(reader)
+        st.divider()
 
-    output_record = []
+        if st.button("Generate Data", type="primary"):
+            global data
+            with open('input.csv', 'r') as f:
+                reader = csv.reader(f, delimiter='|')
+                data = list(reader)
 
-    print(len(data))
-    fake = Faker(['en_US'])
+            output_record = []
 
-    for i in range(1, len(data)):
-        if data[i][0] == "Name":
-            name = nm(fake, records, data[i][2])
-            output_record.append(name)
+            print(len(data))
+            fake = Faker(['en_US'])
 
-        if data[i][0] == "Phone":
-            phone = ph(fake, records, data[i][2])
-            output_record.append(phone)
+            for i in range(1, len(data)):
+                if data[i][0] == "Name":
+                    name = nm(fake, records, data[i][2])
+                    output_record.append(name)
 
-        if data[i][0] == "Email":
-            email = em(fake, records, data[i][2])
-            output_record.append(email)
+                if data[i][0] == "Phone":
+                    phone = ph(fake, records, data[i][2])
+                    output_record.append(phone)
 
-        if data[i][0] == "Amount":
-            amount = amt(records, float(data[i][2]), float(data[i][3]))
-            output_record.append(amount)
+                if data[i][0] == "Email":
+                    email = em(fake, records, data[i][2])
+                    output_record.append(email)
 
-        if data[i][0] == "Text":
-            text = txt(fake, records, data[i][2])
-            output_record.append(text)
+                if data[i][0] == "Amount":
+                    amount = amt(records, float(data[i][2]), float(data[i][3]))
+                    output_record.append(amount)
 
-        if data[i][0] == "Address":
-            address = addrs(fake, records)
-            output_record.append(address)
+                if data[i][0] == "Text":
+                    text = txt(fake, records, data[i][2])
+                    output_record.append(text)
 
-        if data[i][0] == "Date":
-            date = dt(fake, records, str(data[i][2]), str(data[i][3]))
-            output_record.append(date)
+                if data[i][0] == "Address":
+                    address = addrs(fake, records)
+                    output_record.append(address)
 
-        if data[i][0] == "Custom":
-            custom = cstm(records, data[i][2])
-            output_record.append(custom)
+                if data[i][0] == "Date":
+                    date = dt(fake, records, str(data[i][2]), str(data[i][3]))
+                    output_record.append(date)
 
-        if data[i][0] == "Categorical":
-            categorical = ctg(fake, records, data[i][2])
-            output_record.append(categorical)
+                if data[i][0] == "Custom":
+                    custom = cstm(records, data[i][2])
+                    output_record.append(custom)
 
-    # f = open("input.csv", "w")
-    # f.write("datatype,columnName,property1,property2,property3,property4\n")
-    # f.close()
+                if data[i][0] == "Categorical":
+                    categorical = ctg(fake, records, data[i][2])
+                    output_record.append(categorical)
 
-    # print("final_op:")
-    # print(output_record)
-    Tp_output = [['' for x in range(len(output_record))] for y in range(len(output_record[0]))]
+            f = open("input.csv", "w")
+            f.write("datatype|columnName|property1|property2|property3|property4\n")
+            f.close()
 
-    tps(output_record, Tp_output, len(output_record), len(output_record[0]))
+            # print("final_op:")
+            # print(output_record)
+            Tp_output = [['' for x in range(len(output_record))] for y in range(len(output_record[0]))]
 
-    # print("Modified matrix is")
-    st.write(str(Tp_output))
+            tps(output_record, Tp_output, len(output_record), len(output_record[0]))
 
-    myFile = open('output.csv', 'w')
-    writer = csv.writer(myFile)
+            st.write(str(Tp_output))
 
-    for data_list in Tp_output:
-        writer.writerow(data_list)
-    myFile.close()
+            myFile = open('output.csv', 'w')
+            writer = csv.writer(myFile)
+
+            for data_list in Tp_output:
+                writer.writerow(data_list)
+            myFile.close()
+
+    except:
+        e = RuntimeError('Enter a valid number.')
+        st.exception(e)
+
+with tab2:
+    SDUI()
---
+++ SyntheticDataUI.py
@@ -0,0 +1,85 @@
+import pandas as pd
+from ctgan import CTGAN
+from io import StringIO
+import streamlit as st
+
+
+def SDUI():
+    global data
+    global records
+    trig = False
+
+    try:
+        records = int(st.text_input('Number of records to be generated: ', '10', key=100007))
+        trig = True
+
+    except:
+        e = RuntimeError('Enter a valid number.')
+        st.exception(e)
+
+    uploaded_file = st.file_uploader("Choose a file", accept_multiple_files=False)
+    if uploaded_file is not None:
+        # # To read file as bytes:
+        # bytes_data = uploaded_file.getvalue()
+        # # st.write(bytes_data)
+        #
+        # # To convert to a string based IO:
+        # stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        # # st.write(stringio)
+        #
+        # # To read file as string:
+        # string_data = stringio.read()
+        # # st.write(string_data)
+
+        # Can be used wherever a "file-like" object is accepted:
+        data = pd.read_csv(uploaded_file)
+        print(data)
+
+        coltype = data.columns.to_series().groupby(data.dtypes).groups
+        obj = []
+        for i, j in coltype.items():
+            if str(i) == 'object':
+                obj = list(j)
+
+        catCol = st.text_input('Categorical Features: ', '')
+        catCol = catCol.replace(" ", "")
+
+        if catCol == '' and len(obj) <= 0:
+            if trig:
+                if st.button("Generate Data", type="primary", key=100008):
+                    ctgan = CTGAN(verbose=True)
+                    ctgan.fit(data, epochs=200)
+
+                    samples = ctgan.sample(10)
+                    print(samples)
+
+        elif catCol == '' and len(obj) > 0:
+            categorical_features = obj
+            if trig:
+
+                if st.button("Generate Data", type="primary", key=100009):
+                    ctgan = CTGAN(verbose=True)
+                    ctgan.fit(data, categorical_features, epochs=200)
+
+                    samples = ctgan.sample(10)
+                    print(samples)
+
+        else:
+            if len(obj) > 0:
+                for i in obj:
+                    catCol += "," + i
+
+            categorical_features = set(catCol.split(','))
+            categorical_features = list(categorical_features)
+
+            if trig:
+
+                if st.button("Generate Data", type="primary", key=100009):
+                    ctgan = CTGAN(verbose=True)
+                    ctgan.fit(data, categorical_features, epochs=200)
+                    samples = ctgan.sample(10)
+                    print(samples)
+
+    else:
+        e = RuntimeError('Upload a file.')
+        st.exception(e)
